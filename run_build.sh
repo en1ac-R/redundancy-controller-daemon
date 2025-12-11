6891b935bcbdf6be8cfc7fce9cdd732c5c76be48
eniac <eniac_r@vk.com>
2025-12-11 17:37:55 +0600

    Add toolchain cmake & build script

---
+++ run_build.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+script_dir="$(dirname "$(realpath "$0")")"
+
+# Fullpath to SYSROOT
+SYSROOT_PATH=$script_dir/OKT507-C_V2.1_SYSROOT
+
+cmake --fresh -S $script_dir -B $script_dir/_builds -G Ninja \
+  --toolchain=$script_dir/cmake/aarch64_linux.cmake \
+  -DCMAKE_SYSROOT=$SYSROOT_PATH -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  
+
+cmake --build $script_dir/_builds -- -j$(nproc)
